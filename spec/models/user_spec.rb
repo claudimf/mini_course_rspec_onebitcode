d2237905102e1337aa6cc00a29db39f22adcf8aa
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2020-11-23 08:09:04 -0300

    Write model tests in User class using FactoryBot Gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,27 +12,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is invalid if the level is not between 1 an 99" do
-    expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid
-  end
-  it "returns the correct hero title" do
-    user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
-    expect(user.title).to eq('wizard Chronos #1')
-  end
-
-  it "is invalid if the level is not between 1 an 99" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
-    expect(user).to_not be_valid
+  context 'Creating user manually without gem to help' do
+    it "is invalid if the level is not between 1 an 99" do
+      expect(User.create(nickname: 'Chronos', kind: :wizard, level: 100)).to_not be_valid
+    end
+    it "returns the correct hero title" do
+      user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
+      expect(user.title).to eq('wizard Chronos #1')
+    end
   end
 
-  it "returns the correct hero title" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(1..99)
-    user = User.create(nickname: nickname, kind: kind, level: level)
-    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
+  context 'Using FFaker Gem' do
+    it "is invalid if the level is not between 1 an 99" do
+      nickname = FFaker::Name.first_name
+      kind = %i[knight wizard].sample
+      level = FFaker::Random.rand(100..9999)
+      user = User.new(nickname: nickname, kind: kind, level: level)
+      expect(user).to_not be_valid
+    end
+
+    it "returns the correct hero title" do
+      nickname = FFaker::Name.first_name
+      kind = %i[knight wizard].sample
+      level = FFaker::Random.rand(1..99)
+      user = User.create(nickname: nickname, kind: kind, level: level)
+      expect(user.title).to eq("#{kind} #{nickname} ##{level}")
+    end
+  end
+
+  context 'Using FFaker + FactoryBot Gems' do
+    it "is invalid if the level is not between 1 an 99" do
+      user = build(:user, level: FFaker::Random.rand(100..9999))
+      expect(user).to_not be_valid
+    end
+
+    it "returns the correct hero title" do
+      nickname = FFaker::Name.first_name
+      kind = %i[knight wizard].sample
+      level = FFaker::Random.rand(1..99)
+
+      user = create(:user, nickname: nickname, kind: kind, level: level)
+      expect(user.title).to eq("#{kind} #{nickname} ##{level}")
+    end
   end
 end
